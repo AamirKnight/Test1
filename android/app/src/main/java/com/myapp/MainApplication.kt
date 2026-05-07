17077c519159656e31b289f1c0c650bb751f99e6
Aamirheart <aamir@heartitout.in>
2026-05-07 14:42:24 +0530

    addd

--- android/app/src/main/java/com/myapp/MainApplication.kt
+++ android/app/src/main/java/com/myapp/MainApplication.kt
@@ -1,11 +1,17 @@
 package com.myapp
 
 import android.app.Application
+import android.media.AudioAttributes
+
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+
+import com.oney.WebRTCModule.WebRTCModuleOptions
+
+import org.webrtc.audio.JavaAudioDeviceModule
 
 class MainApplication : Application(), ReactApplication {
 
@@ -14,7 +20,7 @@ class MainApplication : Application(), ReactApplication {
       context = applicationContext,
       packageList =
         PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // Packages that cannot be autolinked yet can be added manually here
           // add(MyReactNativePackage())
         },
     )
@@ -22,6 +28,20 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+
+    // Configure WebRTC audio module BEFORE React Native initializes
+    val options = WebRTCModuleOptions.getInstance()
+
+    val audioAttributes = AudioAttributes.Builder()
+      .setUsage(AudioAttributes.USAGE_MEDIA)
+      .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+      .build()
+
+    options.audioDeviceModule =
+      JavaAudioDeviceModule.builder(this)
+        .setAudioAttributes(audioAttributes)
+        .createAudioDeviceModule()
+
     loadReactNative(this)
   }
 }
